916660660f0b9f7bfc7f92c809be599440df6ae8
gqueico <gqueico@gmail.com>
2024-09-12 23:45:35 -0300

    :construction: Work in progress

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+app.post("/users", (request, response) => {
+
+  response.send('Você chamou o POST')
+})
+
+const PORT = 3333
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
